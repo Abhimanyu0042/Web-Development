733481790c4a85250f344f0f1079d542131c6bf4
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2023-02-13 00:06:59 +0530

    Javascript Practice

---
+++ CodeShuttle/Navbar/script.js
@@ -0,0 +1,8 @@
+const toggler = document.getElementById('toggler');
+const navLinks = document.getElementById('nav-links');
+
+toggler.addEventListener('click', ()=>{
+    navLinks.classList.toggle('active');
+
+})
+
